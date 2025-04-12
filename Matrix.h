b1f410b87a3a66c173282888f88ab587838eb22e
Raul <puscas.raul.pr@gmail.com>
2025-04-12 19:14:08 +0300

    it was the source file

--- Matrix.h
+++ Matrix.h
@@ -1,99 +1,26 @@
-#include <stdlib.h>
-#include <string.h>
+#include <stddef.h>
+#include <sys/types.h>
 
-#include "Matrix.h"
+typedef enum {TYPE_STRING, TYPE_INT, TYPE_FLOAT} ColumnType;
+#define MAX_COLUMNS 400 
+#define COLUMN_AS(type, col) ((type*)(col).values)
 
-// free's the matrix and all it's containg columns
-// no return of any sort if anything fails it's seg fault
-void free_matrix(Matrix *matrix) {
-	for (size_t i = 0; i < matrix->size; i++) {
-		switch(matrix->column[i].type) {
-			case TYPE_INT:
-				free(matrix->column[i].values);
-				matrix->column[i].size = 0;
-				matrix->column[i].capacity = -1;
-				continue;
-			case TYPE_FLOAT:
-				free(matrix->column[i].values);
-				matrix->column[i].size = 0;
-				matrix->column[i].capacity = -1;
-				continue;
-			case TYPE_STRING:
-				for (size_t j = 0; j < matrix->column[i].size; j++)
-					free(((char**) matrix->column[i].values)[j]);
-				free(matrix->column[i].values);
-				matrix->column[i].size = 0;
-				matrix->column[i].capacity = -1;
-				continue;
-			default:
-				continue;
-		}
-	}
-}
+typedef struct {
+	ColumnType type;
+	void *values;
+	size_t size;
+	size_t capacity;
+} Column;
 
-// resizes a columns values field to 2 * capacity
-// returns 1 on some sort of error
-// 0 on success
-size_t resize(Column *column) {
-	column->capacity *= 2;
-	switch (column->type) {
-		case TYPE_INT:
-			;
-			int *temp_int_values = 
-				realloc((int*) column->values, column->capacity * sizeof(int));
-			if (!temp_int_values) 
-				return 1;
-			column->values = temp_int_values;
-			return 0;
-		case TYPE_FLOAT:
-			;
-			double *temp_double_values = 
-				realloc((double*) column->values, column->capacity * sizeof(double));
-			if (!temp_double_values)
-				return 1;
-			column->values = temp_double_values;
-			return 0;
-		case TYPE_STRING:
-			;
-			char **temp_string_values =
-				realloc((char**) column->values, column->capacity * sizeof(char*));
-			if (!temp_string_values)
-				return 1;
-			column->values = temp_string_values;
-			return 0;
-		default:
-			return 1;
-	}
-}
+typedef struct {
+	char **key_columns;
+	Column *column;
+	size_t size;
+} Matrix;
 
-size_t add_element(Column *column, void *element) {
-	if (column->size >= column->capacity) 
-		if (resize(column))
-			return 1;
+void free_matrix(Matrix *matrix);
 
-	switch (column->type) {
-		case TYPE_INT:
-			;
-			int *int_value = (int*) element;
-			((int*) (column->values))[column->size] = *int_value;
-			++column->size;
-			return 0;
-		case TYPE_FLOAT:
-			;
-			double *double_value = (double*) element;
-			((double*) (column->values))[column->size] = *double_value;
-			++column->size;
-			return 0;
-		case TYPE_STRING:
-			;
-			char *string = (char*) element;
-			((char**) (column->values))[column->size] = 
-				malloc (sizeof(char) * strlen(string) + 1);
-			memmove(((char**) (column->values))[column->size], 
-					string, strlen(string));
-			++column->size;
-			return 0;
-		default:
-			return 1;
-	}
-}
+size_t resize(Column *column);
+
+size_t add_element(Column *column, void *element); 
+ 
